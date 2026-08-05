ea32361ed0700b706a6d8ff9dfb48d5da31341b1
Vamsi Chinamilli <nagavamsichinamilli@gmail.com>
2026-08-05 20:13:47 +0530

    Fix Django Allowed Hosts(2)

--- WeatherB/core/settings.py
+++ WeatherB/core/settings.py
@@ -33,11 +33,10 @@ DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
 
 
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-    "geospatialai.onrender.com",
-]
+ALLOWED_HOSTS = os.getenv(
+    "ALLOWED_HOSTS",
+    "localhost,127.0.0.1"
+).split(",")
 
 if not DEBUG:
     SECURE_SSL_REDIRECT = True
@@ -72,6 +71,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -183,3 +183,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
