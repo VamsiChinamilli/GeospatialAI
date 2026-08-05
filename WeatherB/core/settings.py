c091afe80abaeced96f5007b2385e34fba79624b
Vamsi Chinamilli <nagavamsichinamilli@gmail.com>
2026-08-05 19:51:37 +0530

    Fix Django Allowed Hosts

--- WeatherB/core/settings.py
+++ WeatherB/core/settings.py
@@ -33,10 +33,11 @@ DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
 
 
-ALLOWED_HOSTS = os.getenv(
-    "ALLOWED_HOSTS",
-    "localhost,127.0.0.1"
-).split(",")
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "geospatialai.onrender.com",
+]
 
 if not DEBUG:
     SECURE_SSL_REDIRECT = True
